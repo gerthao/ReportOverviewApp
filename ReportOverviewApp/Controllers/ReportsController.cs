98b45b372ffb6641f5b29647c136dae29544a56b
Ger Thao <g.thao19@gmail.com>
2017-06-12 16:25:43 -0500

    Add project files.

---
+++ ReportOverviewApp/Controllers/ReportsController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReportOverviewApp.Data;
+using ReportOverviewApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ReportOverviewApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            //if (User == null)
+            //{
+            //    Redirect("../AccessDenied");
+            //    return;
+            //}
+            _context = context;    
+        }
+
+        // GET: Reports
+        [Authorize]
+        public async Task<IActionResult> Index(string search)
+        {
+            var reports = from r in _context.Reports select r;
+            if (!String.IsNullOrEmpty(search))
+            {
+                reports = reports.Where(report => report.Name.Contains(search));
+            }
+            return View(await reports.ToListAsync());
+        }
+
+        // GET: Reports/Details/5
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // GET: Reports/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("ID,Name,Done,DateDone,DateClientNotified,DateSent,ClientNotified,Sent,DateDue")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(report);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports.SingleOrDefaultAsync(m => m.ID == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            return View(report);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Done,DateDone,DateClientNotified,DateSent,ClientNotified,Sent,DateDue")] Report report)
+        {
+            if (id != report.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportExists(report.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // POST: Reports/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var report = await _context.Reports.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Reports.Remove(report);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool ReportExists(int id)
+        {
+            return _context.Reports.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ReportOverviewApp/Models/Report.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportOverviewApp.Models
+{
+    public class Report
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+
+        [Display(Name = "Report Finished"), DataType(DataType.DateTime)]
+        public DateTime? DateDone { get; set; }
+        [Display(Name = "Client Notified"), DataType(DataType.DateTime)]
+        public DateTime? DateClientNotified { get; set; }
+        [Display(Name = "Report Sent"), DataType(DataType.DateTime)]
+        public DateTime? DateSent { get; set; }
+        public bool ClientNotified { get; set; }
+        public bool Sent { get; set; }
+
+        [Display(Name = "Due Date"), DataType(DataType.Date)]
+        public DateTime DateDue { get; set; }
+    }
+}
