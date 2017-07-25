db6c7896c5b570119a49878c207c5e51ad39b830
Ger Thao <g.thao19@gmail.com>
2017-07-25 09:29:52 -0500

    Needed to fix an issue.

--- ReportOverviewApp/Models/Report_Delivery_TrackingContext.cs
+++ ReportOverviewApp/Models/Report_Delivery_TrackingContext.cs
@@ -11,7 +11,7 @@ namespace ReportOverviewApp.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-            optionsBuilder.UseSqlServer(@"Data Source=wadtisql01d.dqdev.ad;Initial Catalog=Report_Delivery_Tracking;Integrated Security=False;User ID=ETL_Developer;Password=3RR_$dev;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
