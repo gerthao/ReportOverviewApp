caed70b8c1d2d1f6e45af4997e8ff42dcdef44b5
Ger Thao <g.thao19@gmail.com>
2018-02-15 17:25:58 -0600

    Added documentation page.  Changed UI layout to not use container class from _Layout to be more flexible with ui.

---
+++ ReportOverviewApp/Controllers/DocsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportOverviewApp.Controllers
+{
+    
+    public class DocsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult Plans()
+        {
+            return View();
+        }
+        public IActionResult BusinessContacts()
+        {
+            return View();
+        }
+    }
+}
